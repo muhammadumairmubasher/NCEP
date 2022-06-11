9f853c5543f59d534d34f9c610de558981982290
MUHAMMAD UMAIR MUBASHER <Muhamadumair7445@gmail.com>
2022-06-12 01:58:24 +0500

    Delete client directory

--- client/src/components/GlobalFilter.jsx
+++
@@ -1,9 +0,0 @@
-import react from 'react'
-export const GlobalFilter=({filter,setFilter})=>{
-    return (
-        <span>
-            search:{''}
-            <input value={filter ||'' } onChange={(e)=>setFilter(e.target.value)} />
-        </span>
-    )
-}
--- client/src/components/NotFound.jsx
+++
@@ -1,7 +0,0 @@
-import notfound from '../assets/img/NotFound.jpg';
-const NotFound = () => {
-    return (
-        <img src={notfound} alt='Not Found' style={{ width: '30%', margin: '80px 0 0 35%' }} />
-    )
-}
-export default NotFound;
